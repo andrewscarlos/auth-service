6ef4d92c537b2a52c6f634f145abb694314417f7
andrewscarlos <andrews.knct@gmail.com>
2023-03-20 22:23:22 -0300

    remove gitignore files

--- dist/auth/auth.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { AuthService } from './auth.service';
-import { AuthRequest } from './interfaces/AuthResquest.interface';
-export declare class AuthController {
-    private readonly authService;
-    constructor(authService: AuthService);
-    login(req: AuthRequest): Promise<import("./interfaces/UserToken.interface").UserToken>;
-}
--- dist/user/dto/create-user.dto.d.ts
+++
@@ -1,6 +0,0 @@
-import { User } from '../entities/user.entity';
-export declare class CreateUserDto extends User {
-    email: string;
-    password: string;
-    name: string;
-}
